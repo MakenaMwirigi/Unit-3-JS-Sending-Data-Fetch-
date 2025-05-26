525da675ea59b51a3553aeba2014ecc7cb2c9ea8
MakenaMwirigi <makenasharon46@gmail.com>
2025-05-26 15:29:49 +0300

    sending data with fetch exercise

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData (name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    })
+    .then(response => response.json())
+    .then (data => {
+        const p = document.createElement("p");
+        p.textContent = `ID: ${data.id}`;
+        document.body.appendChild(p);
+    })
+    .catch(error => {
+      const p = document.createElement("p");
+      p.textContent = `Error: ${error.message}`;
+      document.body.appendChild(p);
+    });
+}
